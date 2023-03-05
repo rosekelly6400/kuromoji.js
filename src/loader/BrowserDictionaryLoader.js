70397e878bde49ad16458abf2f5eb87f27979160
rosekelly6400 <109883728+rosekelly6400@users.noreply.github.com>
2023-03-04 19:23:06 -0500

    dont reference commented out var

--- src/loader/BrowserDictionaryLoader.js
+++ src/loader/BrowserDictionaryLoader.js
@@ -49,7 +49,7 @@ BrowserDictionaryLoader.prototype.loadArrayBuffer = function (url, callback) {
 
         // var gz = new zlib.Zlib.Gunzip(new Uint8Array(arraybuffer));
         // var typed_array = gz.decompress();
-        callback(null, typed_array.buffer);
+        callback(null, arraybuffer);
     };
     xhr.onerror = function (err) {
         callback(err, null);
